1c18a61965b226528c404a6d3f8d90c40050b9a3
JoshDS595 <joshuadasilva595@gmail.com>
2023-11-08 20:16:39 -0500

    more stuff

--- customelement.js
+++ customelement.js
@@ -5,3 +5,11 @@ class JoshElement extends HTMLElement{
 }
 
 customElements.define("x-josh/, JoshElement");
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = "Josh was here";
+    }
+}
+
+customElements.define("x-center/, CenterElement");
